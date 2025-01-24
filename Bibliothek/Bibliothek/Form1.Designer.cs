948ec52f87e528d98fc383d02bfc6907fa4ca398
xXGrowGuruXx <116651235+xXGrowGuruXx@users.noreply.github.com>
2025-01-24 10:07:37 +0100

    Create Project
    - Login Form
    . Name auf Login geändert
    . Minimize und Maximize deaktiviert
    . StartPosition auf Center gestellt
    . StandartSchriftgröße auf 12
    . Sizeable auf FixedSingle

---
+++ Bibliothek/Bibliothek/Form1.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace Bibliothek
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(11F, 28F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(693, 400);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(4, 4, 4, 4);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Bibliothek/Bibliothek/Form1.cs
@@ -0,0 +1,10 @@
+namespace Bibliothek
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
