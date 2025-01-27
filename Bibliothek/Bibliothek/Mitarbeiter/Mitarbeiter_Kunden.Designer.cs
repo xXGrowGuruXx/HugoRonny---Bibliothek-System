7f37aba888832f25779af6dd40ef4cf949bbe909
OnkelDoc24 <Peters.r24h@gmail.com>
2025-01-27 09:57:57 +0100

    Kunden bearbeiten
    Kunden bearbeiten

---
+++ Bibliothek/Bibliothek/Mitarbeiter/Mitarbeiter_Kunden.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Bibliothek.Mitarbeiter
+{
+    partial class Mitarbeiter_Kunden
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Mitarbeiter_Kunden));
+            mitarbeiter_Name = new Label();
+            Mitarbeiter_Kunden_Zurueck = new Button();
+            login_Label_Username = new Label();
+            login_Username = new TextBox();
+            Mitarbeiter_Kunden_neu_comboBox = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            Mitarbeiter_Kunden_Ausgeliehen = new Button();
+            Mitarbeiter_Kunden_Reservierungen = new Button();
+            Mitarbeiter_Kunden_Speichern = new Button();
+            Mitarbeiter_Kunden_Deaktivieren = new Button();
+            SuspendLayout();
+            // 
+            // mitarbeiter_Name
+            // 
+            mitarbeiter_Name.AutoSize = true;
+            mitarbeiter_Name.BackColor = Color.Transparent;
+            mitarbeiter_Name.Font = new Font("Microsoft Sans Serif", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            mitarbeiter_Name.ForeColor = Color.Transparent;
+            mitarbeiter_Name.Location = new Point(265, 9);
+            mitarbeiter_Name.Name = "mitarbeiter_Name";
+            mitarbeiter_Name.Size = new Size(143, 37);
+            mitarbeiter_Name.TabIndex = 6;
+            mitarbeiter_Name.Text = "Kunden ";
+            mitarbeiter_Name.TextAlign = ContentAlignment.MiddleCenter;
+            mitarbeiter_Name.Click += mitarbeiter_Name_Click;
+            // 
+            // Mitarbeiter_Kunden_Zurueck
+            // 
+            Mitarbeiter_Kunden_Zurueck.BackColor = Color.Transparent;
+            Mitarbeiter_Kunden_Zurueck.FlatStyle = FlatStyle.Popup;
+            Mitarbeiter_Kunden_Zurueck.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Mitarbeiter_Kunden_Zurueck.ForeColor = SystemColors.ButtonHighlight;
+            Mitarbeiter_Kunden_Zurueck.Location = new Point(464, 341);
+            Mitarbeiter_Kunden_Zurueck.Margin = new Padding(0);
+            Mitarbeiter_Kunden_Zurueck.Name = "Mitarbeiter_Kunden_Zurueck";
+            Mitarbeiter_Kunden_Zurueck.Size = new Size(211, 76);
+            Mitarbeiter_Kunden_Zurueck.TabIndex = 7;
+            Mitarbeiter_Kunden_Zurueck.Text = "Zurück";
+            Mitarbeiter_Kunden_Zurueck.UseVisualStyleBackColor = false;
+            // 
+            // login_Label_Username
+            // 
+            login_Label_Username.AutoSize = true;
+            login_Label_Username.BackColor = Color.Transparent;
+            login_Label_Username.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            login_Label_Username.ForeColor = SystemColors.ButtonHighlight;
+            login_Label_Username.Location = new Point(39, 94);
+            login_Label_Username.Name = "login_Label_Username";
+            login_Label_Username.Size = new Size(91, 20);
+            login_Label_Username.TabIndex = 8;
+            login_Label_Username.Text = "Username";
+            // 
+            // login_Username
+            // 
+            login_Username.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            login_Username.Location = new Point(187, 88);
+            login_Username.Name = "login_Username";
+            login_Username.Size = new Size(251, 26);
+            login_Username.TabIndex = 9;
+            login_Username.TextAlign = HorizontalAlignment.Center;
+            // 
+            // Mitarbeiter_Kunden_neu_comboBox
+            // 
+            Mitarbeiter_Kunden_neu_comboBox.FormattingEnabled = true;
+            Mitarbeiter_Kunden_neu_comboBox.Location = new Point(39, 51);
+            Mitarbeiter_Kunden_neu_comboBox.Name = "Mitarbeiter_Kunden_neu_comboBox";
+            Mitarbeiter_Kunden_neu_comboBox.Size = new Size(156, 23);
+            Mitarbeiter_Kunden_neu_comboBox.TabIndex = 10;
+            Mitarbeiter_Kunden_neu_comboBox.Text = "Kunde auswählen";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(39, 133);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 20);
+            label1.TabIndex = 11;
+            label1.Text = "Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(39, 171);
+            label2.Name = "label2";
+            label2.Size = new Size(81, 20);
+            label2.TabIndex = 12;
+            label2.Text = "Vorname";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(39, 209);
+            label3.Name = "label3";
+            label3.Size = new Size(71, 20);
+            label3.TabIndex = 13;
+            label3.Text = "Strasse";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ButtonHighlight;
+            label4.Location = new Point(39, 248);
+            label4.Name = "label4";
+            label4.Size = new Size(34, 20);
+            label4.TabIndex = 14;
+            label4.Text = "Ort";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ButtonHighlight;
+            label5.Location = new Point(39, 287);
+            label5.Name = "label5";
+            label5.Size = new Size(41, 20);
+            label5.TabIndex = 15;
+            label5.Text = "PLZ";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(187, 127);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(251, 26);
+            textBox1.TabIndex = 16;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(187, 165);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(251, 26);
+            textBox2.TabIndex = 17;
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(187, 203);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(251, 26);
+            textBox3.TabIndex = 18;
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(187, 242);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(251, 26);
+            textBox4.TabIndex = 19;
+            textBox4.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox5.Location = new Point(187, 281);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(251, 26);
+            textBox5.TabIndex = 20;
+            textBox5.TextAlign = HorizontalAlignment.Center;
+            // 
+            // Mitarbeiter_Kunden_Ausgeliehen
+            // 
+            Mitarbeiter_Kunden_Ausgeliehen.BackColor = Color.Transparent;
+            Mitarbeiter_Kunden_Ausgeliehen.FlatStyle = FlatStyle.Popup;
+            Mitarbeiter_Kunden_Ausgeliehen.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Mitarbeiter_Kunden_Ausgeliehen.ForeColor = SystemColors.ButtonHighlight;
+            Mitarbeiter_Kunden_Ausgeliehen.Location = new Point(9, 341);
+            Mitarbeiter_Kunden_Ausgeliehen.Margin = new Padding(0);
+            Mitarbeiter_Kunden_Ausgeliehen.Name = "Mitarbeiter_Kunden_Ausgeliehen";
+            Mitarbeiter_Kunden_Ausgeliehen.Size = new Size(186, 76);
+            Mitarbeiter_Kunden_Ausgeliehen.TabIndex = 21;
+            Mitarbeiter_Kunden_Ausgeliehen.Text = "Ausgeliehen anzeigen";
+            Mitarbeiter_Kunden_Ausgeliehen.UseVisualStyleBackColor = false;
+            // 
+            // Mitarbeiter_Kunden_Reservierungen
+            // 
+            Mitarbeiter_Kunden_Reservierungen.BackColor = Color.Transparent;
+            Mitarbeiter_Kunden_Reservierungen.FlatStyle = FlatStyle.Popup;
+            Mitarbeiter_Kunden_Reservierungen.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Mitarbeiter_Kunden_Reservierungen.ForeColor = SystemColors.ButtonHighlight;
+            Mitarbeiter_Kunden_Reservierungen.Location = new Point(220, 341);
+            Mitarbeiter_Kunden_Reservierungen.Margin = new Padding(0);
+            Mitarbeiter_Kunden_Reservierungen.Name = "Mitarbeiter_Kunden_Reservierungen";
+            Mitarbeiter_Kunden_Reservierungen.Size = new Size(229, 76);
+            Mitarbeiter_Kunden_Reservierungen.TabIndex = 22;
+            Mitarbeiter_Kunden_Reservierungen.Text = "Reservierungen anzeigen";
+            Mitarbeiter_Kunden_Reservierungen.UseVisualStyleBackColor = false;
+            // 
+            // Mitarbeiter_Kunden_Speichern
+            // 
+            Mitarbeiter_Kunden_Speichern.BackColor = Color.Transparent;
+            Mitarbeiter_Kunden_Speichern.FlatStyle = FlatStyle.Popup;
+            Mitarbeiter_Kunden_Speichern.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Mitarbeiter_Kunden_Speichern.ForeColor = SystemColors.ButtonHighlight;
+            Mitarbeiter_Kunden_Speichern.Location = new Point(464, 88);
+            Mitarbeiter_Kunden_Speichern.Margin = new Padding(0);
+            Mitarbeiter_Kunden_Speichern.Name = "Mitarbeiter_Kunden_Speichern";
+            Mitarbeiter_Kunden_Speichern.Size = new Size(211, 124);
+            Mitarbeiter_Kunden_Speichern.TabIndex = 23;
+            Mitarbeiter_Kunden_Speichern.Text = "Speichern";
+            Mitarbeiter_Kunden_Speichern.UseVisualStyleBackColor = false;
+            // 
+            // Mitarbeiter_Kunden_Deaktivieren
+            // 
+            Mitarbeiter_Kunden_Deaktivieren.BackColor = Color.Transparent;
+            Mitarbeiter_Kunden_Deaktivieren.FlatStyle = FlatStyle.Popup;
+            Mitarbeiter_Kunden_Deaktivieren.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Mitarbeiter_Kunden_Deaktivieren.ForeColor = SystemColors.ButtonHighlight;
+            Mitarbeiter_Kunden_Deaktivieren.Location = new Point(464, 231);
+            Mitarbeiter_Kunden_Deaktivieren.Margin = new Padding(0);
+            Mitarbeiter_Kunden_Deaktivieren.Name = "Mitarbeiter_Kunden_Deaktivieren";
+            Mitarbeiter_Kunden_Deaktivieren.Size = new Size(211, 76);
+            Mitarbeiter_Kunden_Deaktivieren.TabIndex = 24;
+            Mitarbeiter_Kunden_Deaktivieren.Text = "Konto deaktivieren";
+            Mitarbeiter_Kunden_Deaktivieren.UseVisualStyleBackColor = false;
+            // 
+            // Mitarbeiter_Kunden
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.Hintergrund800x600;
+            ClientSize = new Size(684, 426);
+            Controls.Add(Mitarbeiter_Kunden_Deaktivieren);
+            Controls.Add(Mitarbeiter_Kunden_Speichern);
+            Controls.Add(Mitarbeiter_Kunden_Reservierungen);
+            Controls.Add(Mitarbeiter_Kunden_Ausgeliehen);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(Mitarbeiter_Kunden_neu_comboBox);
+            Controls.Add(login_Username);
+            Controls.Add(login_Label_Username);
+            Controls.Add(Mitarbeiter_Kunden_Zurueck);
+            Controls.Add(mitarbeiter_Name);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Mitarbeiter_Kunden";
+            Text = "Mitarbeiter_Kunden";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label mitarbeiter_Name;
+        private Button Mitarbeiter_Kunden_Zurueck;
+        private Label login_Label_Username;
+        private TextBox login_Username;
+        private ComboBox Mitarbeiter_Kunden_neu_comboBox;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Button Mitarbeiter_Kunden_Ausgeliehen;
+        private Button Mitarbeiter_Kunden_Reservierungen;
+        private Button Mitarbeiter_Kunden_Speichern;
+        private Button Mitarbeiter_Kunden_Deaktivieren;
+    }
+}
