4f3970451bfc7aa58a15972b5396917f01a816a3
xXGrowGuruXx <116651235+xXGrowGuruXx@users.noreply.github.com>
2025-01-29 11:52:50 +0100

    gitignore

--- Bibliothek/Bibliothek/obj/Debug/net8.0-windows/Bibliothek.AssemblyInfo.cs
+++ Bibliothek/Bibliothek/obj/Debug/net8.0-windows/Bibliothek.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bibliothek")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96a841a84c2c6bec35e33a7036af6717262b21d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9912bf77e6774abb15f76069efecd922cccae83")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bibliothek")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bibliothek")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
